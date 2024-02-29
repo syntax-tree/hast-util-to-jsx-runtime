0d29e4c690a5e7fd84e5694c855f4c0cfcd3afb1
Remco Haszing <remcohaszing@gmail.com>
2024-02-29 10:38:40 +0100

    Remove augmentation of React JSX runtime
    The pull request to support this has been merged upstream.

--- types.d.ts
+++ types.d.ts
@@ -1,31 +1,3 @@
-// To do: remove when landed in DT
-// https://github.com/DefinitelyTyped/DefinitelyTyped/pull/68600
-declare module 'react/jsx-runtime' {
-  import {ElementType, Fragment, Key, ReactElement} from 'react'
-
-  function jsx(type: ElementType, props: unknown, key?: Key): ReactElement
-
-  export {Fragment, jsx, jsx as jsxs}
-}
-
-// To do: remove when landed in DT
-// https://github.com/DefinitelyTyped/DefinitelyTyped/pull/68600
-declare module 'react/jsx-dev-runtime' {
-  import {ElementType, Fragment, Key, ReactElement} from 'react'
-  import {Source} from 'hast-util-to-jsx-runtime'
-
-  function jsxDEV(
-    type: ElementType,
-    props: unknown,
-    key: Key | undefined,
-    isStatic: boolean,
-    source?: Source,
-    self?: unknown
-  ): ReactElement
-
-  export {Fragment, jsxDEV}
-}
-
 // Support loading hastscript from https://esm.sh
 declare module 'https://esm.sh/hastscript@8?dev' {
   export * from 'hastscript'
